e4dfb9cc6801976c2c25199210728c1d4acab2e5
Kaopang <kaopang@cris.ac.th>
2026-02-03 10:07:46 +0700

    content and decoration

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+let firstName = "Pornlaphat";
+let lastName = "Wu";
+let fullName = firstName + " " + lastName;
+
+document.getElementById("fullName").innerText = fullName;
+
+
+let age = 14;
+let futureAge = "In twenty years, I will be " + (age + 20) + " years old.";
+
+document.getElementById("age").innerText = age;
+document.getElementById("futureAge").innerText = futureAge;
